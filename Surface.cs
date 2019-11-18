06e9bd617156a7fa8ccb6d42d200c3c23f282d68
Daishikofy <48716129+Daishikofy@users.noreply.github.com>
2019-11-18 16:54:14 -0300

    Add files via upload

---
+++ Surface.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class Surface : MonoBehaviour
+{
+
+    [SerializeField]
+    private int xSize, ySize;
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Awake()
+    {
+        vertices = new Vector3[(xSize + 1)*(ySize + 1)];
+        triangles = new int[xSize * ySize * 6];
+        generate();
+    }
+
+    private void generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+            }
+        }
+        mesh.vertices = vertices;
+
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        Gizmos.color = Color.black;
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+
+    }
+}
